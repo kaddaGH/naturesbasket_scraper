1df990b9ae6193fa0c1cb55f8ec97693add5c1d0
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-03-01 10:53:16 +0100

    Update scraper.

---
+++ lib/headers.rb
@@ -0,0 +1,19 @@
+module ReqHeaders
+  REQ_HEADER = {
+      "accept" => "*/*",
+      "accept-encoding" => "gzip, deflate, br",
+      "accept-language" => "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,de-DE;q=0.6,de;q=0.5",
+      "content-length" => "0",
+      "cookie" => "nbloc=cty=5RcKKNiL6+/EoSIeGL7/Pg==&ctyid=Vthxifmkz8MHpo2bEeFNUQ==&nwidn=EBRObxuhyKVsxpNatCPZ0g==&pin=K1HxGPLeTM5O0sszgRdaIQ==&ispn=C2W0fZgPy0KdDuCafMusSg==;",
+      "origin" => "https://www.naturesbasket.co.in",
+      "referer" => "https://www.naturesbasket.co.in/Online-grocery-shopping/Snacks---Beverages/Energy-Drinks---Milk-Additives/81_0_0",
+      "user-agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36",
+      "x-requested-with" => "XMLHttpRequest"
+  }
+
+end
+
+
+
+
+
--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -1,8 +1,10 @@
 require 'uri'
+require './lib/headers'
 
 pages << {
     page_type: 'products_listing',
     method: 'POST',
+    headers: ReqHeaders::REQ_HEADER,
     url: "https://www.naturesbasket.co.in/Handlers/SearchHandler.ashx?GetMoreSearchData=True&Keyword=&SupercategoryID=0&CategoryID=81&SubCategoryID=0&BrandID=&AttributeValueID=0&fromPrice=0&toPrice=400000&fromSize=1&toSize=100&UnitID=1&fromCount=1&SortID=1&DietaryId=&IsOutOfStock=&IsNewProduct=",
     vars: {
         'input_type' => 'taxonomy',
@@ -18,6 +20,7 @@ search_terms.each do |search_term|
   pages << {
       page_type: 'products_listing',
       method: 'POST',
+      headers: ReqHeaders::REQ_HEADER,
       url: "https://www.naturesbasket.co.in/Handlers/SearchHandler.ashx?GetMoreSearchData=True&Keyword=#{URI.encode(search_term)}&SupercategoryID=0&CategoryID=0&SubCategoryID=0&BrandID=&AttributeValueID=0&fromPrice=0&toPrice=400000&fromSize=1&toSize=100&UnitID=1&fromCount=1&SortID=1&DietaryId=&IsOutOfStock=&IsNewProduct=",
       vars: {
           'input_type' => 'search',
