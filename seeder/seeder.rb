b40c88ce94a900647b4a3904424c0b101610edc0
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-03-01 10:31:07 +0100

    Update scraper.

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -2,7 +2,7 @@ require 'uri'
 
 pages << {
     page_type: 'products_listing',
-    method: 'GET',
+    method: 'POST',
     url: "https://www.naturesbasket.co.in/Handlers/SearchHandler.ashx?GetMoreSearchData=True&Keyword=&SupercategoryID=0&CategoryID=81&SubCategoryID=0&BrandID=&AttributeValueID=0&fromPrice=0&toPrice=400000&fromSize=1&toSize=100&UnitID=1&fromCount=1&SortID=1&DietaryId=&IsOutOfStock=&IsNewProduct=",
     vars: {
         'input_type' => 'taxonomy',
@@ -17,7 +17,7 @@ search_terms = ["Red Bull", "RedBull", "Energy Drink", "Energy Drinks"]
 search_terms.each do |search_term|
   pages << {
       page_type: 'products_listing',
-      method: 'GET',
+      method: 'POST',
       url: "https://www.naturesbasket.co.in/Handlers/SearchHandler.ashx?GetMoreSearchData=True&Keyword=#{URI.encode(search_term)}&SupercategoryID=0&CategoryID=0&SubCategoryID=0&BrandID=&AttributeValueID=0&fromPrice=0&toPrice=400000&fromSize=1&toSize=100&UnitID=1&fromCount=1&SortID=1&DietaryId=&IsOutOfStock=&IsNewProduct=",
       vars: {
           'input_type' => 'search',
