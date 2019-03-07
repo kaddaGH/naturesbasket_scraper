060732d3957ac392120bd6eddec52a9bd63cfaa6
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-03-07 11:22:00 +0100

    update parser.

---
+++ parsers/products_listing.rb
@@ -0,0 +1,56 @@
+require './lib/headers'
+
+products_ids = content.scan(/(?<=pro-id_)(.+?)(?=')/)
+
+scrape_url_nbr_products = content[/\d+\Z/]
+
+next_page_offset = content[/(?<=getSearchData\()\d+(?=\))/]
+current_page = page['vars']['page']
+if current_page==1
+  scrape_url_nbr_products_pg1 =  products_ids.length
+else
+  scrape_url_nbr_products_pg1 =  page['vars']['SCRAPE_URL_NBR_PRODUCTS_PG1']
+
+end
+
+products_ids.each_with_index do |product_id, i|
+
+  pages << {
+      page_type: 'product_details',
+      method: 'GET',
+      url: "https://www.naturesbasket.co.in/Handlers/PopupProductDetailHandler.ashx?FillProductDetail=true&ProductVariantID=#{product_id[0]}&loadtype=popup&search=#{page['vars']['search_term']}&page=#{page['vars']['page']}&rank=#{i + 1}",
+      vars: {
+          'input_type' => page['vars']['input_type'],
+          'search_term' => page['vars']['search_term'],
+          'SCRAPE_URL_NBR_PRODUCTS' => scrape_url_nbr_products,
+          'SCRAPE_URL_NBR_PRODUCTS_PG1' => scrape_url_nbr_products_pg1,
+          'rank' => i + 1,
+          'page' => page['vars']['page']
+      }
+
+  }
+
+
+end
+
+
+# get next page
+if next_page_offset
+
+  pages << {
+      page_type: 'products_listing',
+      method: 'POST',
+      headers: ReqHeaders::REQ_HEADER,
+      url: page['url'].gsub(/fromCount=\d+/,"fromCount="+next_page_offset) ,
+      vars: {
+          'input_type' => page['vars']['input_type'],
+          'search_term' => page['vars']['search_term'],
+          'SCRAPE_URL_NBR_PRODUCTS' => scrape_url_nbr_products,
+          'SCRAPE_URL_NBR_PRODUCTS_PG1' => scrape_url_nbr_products_pg1,
+          'page' => current_page+1
+      }
+
+
+  }
+
+end
