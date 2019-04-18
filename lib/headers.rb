83ac9dd55f12685e2c719f225d674e80cf4834fe
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-04-18 15:14:59 +0100

    Update header.

--- lib/headers.rb
+++ lib/headers.rb
@@ -4,7 +4,7 @@ module ReqHeaders
       "accept-encoding" => "gzip, deflate, br",
       "accept-language" => "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,de-DE;q=0.6,de;q=0.5",
       "content-length" => "0",
-      "cookie" => "RecommendationCookie=usersessionidrec=fc9a437e-6f7b-4d06-adc4-5386b5bf8f07; _fbp=fb.2.1551347047027.1860419539; _ga=GA1.3.534128713.1551347048; nbloc=cty=BZzOtPEghhxiv9GvzPo/Jg==&ctyid=6QOqoV6reu4JL6++H5Q3aw==&nwidn=6QOqoV6reu4JL6++H5Q3aw==&pin=K1HxGPLeTM5O0sszgRdaIQ==&ispn=C2W0fZgPy0KdDuCafMusSg==; ASP.NET_SessionId=55j441kzulekrt0uepl3snu3; mobileCookie=device=user; absource=head; _gid=GA1.3.403274229.1555317772; _gat=1",
+      "cookie" => "RecommendationCookie=usersessionidrec=fc9a437e-6f7b-4d06-adc4-5386b5bf8f07; _fbp=fb.2.1551347047027.1860419539; _ga=GA1.3.534128713.1551347048; nbloc=cty=BZzOtPEghhxiv9GvzPo/Jg==&ctyid=6QOqoV6reu4JL6++H5Q3aw==&nwidn=6QOqoV6reu4JL6++H5Q3aw==&pin=K1HxGPLeTM5O0sszgRdaIQ==&ispn=C2W0fZgPy0KdDuCafMusSg==; mobileCookie=device=user; inactivity=1; ASP.NET_SessionId=ya4m33kltom3xflrnauvfioq; absource=search; _gid=GA1.3.1655829029.1555596121",
       "origin" => "https://www.naturesbasket.co.in",
       "referer" => "https://www.naturesbasket.co.in/Online-grocery-shopping/Snacks---Beverages/Energy-Drinks---Milk-Additives/81_0_0",
       "user-agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36",
